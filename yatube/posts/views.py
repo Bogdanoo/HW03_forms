68e462f2a9990d566de12a54cbb1364b6b6ab666
Bogdan Zhovnir <bogdanzhovnir@gmail.com>
2022-06-03 11:15:48 +0200

    HWFORMS

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,8 +1,9 @@
-from django.core.paginator import Paginator
 from django.conf import settings
-from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import Group, Post, User, PostForm
+from django.core.paginator import Paginator
+from django.shortcuts import get_object_or_404, redirect, render
+
+from .models import Group, Post, PostForm, User
 
 
 def index(request):
