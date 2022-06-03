81860f9c067c8399ca53d97dfe4acc92f13fc895
Bogdan Zhovnir <bogdanzhovnir@gmail.com>
2022-06-03 11:15:21 +0200

    HWFORMS

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -3,13 +3,12 @@ from django.contrib import admin
 from .models import Post, Group
 
 
+@admin.register(Group)
 class GroupAdmin(admin.ModelAdmin):
     list_display = ('pk', 'title', 'slug', 'description')
 
 
-admin.site.register(Group, GroupAdmin)
-
-
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = (
         'pk',
@@ -22,6 +21,3 @@ class PostAdmin(admin.ModelAdmin):
     search_fields = ('text',)
     list_filter = ('pub_date',)
     empty_value_display = '-пусто-'
-
-
-admin.site.register(Post, PostAdmin)
